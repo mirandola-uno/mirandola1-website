a595272dce4c6a7f2f1715d1ecd59b3e088aad04
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2025-08-25 11:53:10 +0200

    feat: improved vertical spacing

--- src/components/news-card.tsx
+++ src/components/news-card.tsx
@@ -1,6 +1,6 @@
 export const NewsCard = ({ title, children }: { title: string; children: React.ReactNode }) => {
   return (
-    <div className="border border-md rounded-lg p-4 w-full md:w-md">
+    <div className="border border-md rounded-lg p-4 w-full md:w-md flex flex-col gap-2">
       <div className="text-3xl font-bold">{title}</div>
       <div className="w-16 h-2 bg-agesci-yellow rounded-md" />
       {children}
