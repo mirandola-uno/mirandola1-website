af0a32d186b1f9e494a1c438d359ff9ec4f04b8c
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2025-08-25 16:20:57 +0200

    feat: browser colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import ClientSideLayout from "./clientside-layout";
 import { ThemeProvider } from "next-themes";
 import { Analytics } from "@vercel/analytics/next";
+import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "Gruppo Scout Mirandola 1",
@@ -16,9 +17,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <meta name="theme-color" content="#462678" />
-      </head>
+      <Head>
+        {/* For Safari on iOS */}
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+
+        {/* For Safari top bar (iOS 15+) */}
+        <meta name="theme-color" content="#462678" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#462678" media="(prefers-color-scheme: dark)" />
+        <meta name="theme-color" content="#462678" media="(prefers-color-scheme: no-preference)" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
+        <meta name="apple-mobile-web-app-title" content="Gruppo Scout Mirandola 1" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
           <ClientSideLayout>{children}</ClientSideLayout>
