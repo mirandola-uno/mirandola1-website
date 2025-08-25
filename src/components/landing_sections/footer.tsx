d31a82c12a10b2a41ed9950d3fc1a770d7de322d
Giacomo Cavicchioli <cavicchioli.giacomo@gmail.com>
2025-08-25 12:43:46 +0200

    feat: improved footer layout

--- src/components/landing_sections/footer.tsx
+++ src/components/landing_sections/footer.tsx
@@ -1,15 +1,24 @@
+import Image from "next/image";
+
 export const FooterSection = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between gap-4 items-center bg-agesci-violet text-white p-4">
-      <div className="flex flex-col gap-1 text-sm">
-        <div className="font-boldx">AGESCI - Gruppo Scout Mirandola 1</div>
+    <div className="flex flex-col-reverse md:flex-row justify-between gap-4 items-center bg-agesci-violet text-white p-2 md:p-8 ">
+      <div className="flex flex-col gap-2 text-sm items-center md:items-start">
+        <Image
+          src="/logo_agesci_white.webp"
+          className="w-10 h-10"
+          alt="Gruppo Scout Mirandola 1"
+          width={50}
+          height={50}
+        />
+        <div className="font-bold">AGESCI - Gruppo Scout Mirandola 1</div>
         <div>C.F. / P.IVA 91027610368</div>
         <div>Via Posta, 55 - 41037 Mirandola (MO)</div>
         <div>Sede Legale: Via Cavour, 2 - 41037 Mirandola (MO)</div>
         <div className="text-muted-foreground">© {new Date().getFullYear()} - Tutti i diritti riservati</div>
       </div>
 
-      <div className="text-3xl font-delius">
+      <div className="text-3xl font-delius text-center md:text-right">
         Del nostro meglio, <br /> per essere pronti a servire.
       </div>
     </div>
